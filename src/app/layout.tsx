ea7de6d7396e7eed9c7b04f5e4aa3b717baab7a2
Abhiro0p <09abhiroop@gmail.com>
2025-12-20 19:56:02 +0530

    force dark mode in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} font-sans antialiased`}>
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body className={`${inter.variable} font-sans antialiased bg-black text-white`}>
         {children}
       </body>
     </html>
